a06b580179630588a7d9d64efa7a04daa2373127
Vesna Groselj <vgroselj@gmail.com>
2024-07-30 01:53:47 +0200

    Add drush command to enable debug and disable debug

--- src/Commands/HtmxPlusCommands.php
+++ src/Commands/HtmxPlusCommands.php
@@ -2,38 +2,51 @@
 
 declare(strict_types=1);
 
-namespace Drupal\htmx_plus\Commands;
+namespace Drupal\htmx_plus\Drush\Commands;
 
+use Drupal\Core\State\StateInterface;
+use Drush\Attributes as CLI;
+use Drush\Commands\AutowireTrait;
 use Drush\Commands\DrushCommands;
 
 /**
  * Drush commands for the HTMX Plus module.
  */
-class HtmxPlusCommands extends DrushCommands {
+final class HtmxPlusCommands extends DrushCommands {
+
+  use AutowireTrait;
+
+  public function __construct(private StateInterface $state) {}
 
   /**
-   * Toggle hx-debug attribute on all htmx elements.
+   * Toggle ext attribute with value debug on all htmx elements.
    *
-   * @param string $enable
-   *   Whether to enable or disable the debug attribute. 'enable' or 'disable'.
-   *
-   * @command htmx_plus:toggle-debug.
-   *
-   * @usage htmx_plus:toggle-debug enable
-   *   Enable the hx-debug attribute on all htmx elements.
-   * @usage htmx_plus:toggle-debug disable
-   *   Disable the hx-debug attribute on all htmx elements.
+   * @param string $mode
+   *   The mode to set, either 'on' or 'off'.
    */
-  public function toggleDebug(string $enable): void {
-    if ($enable === 'enable') {
+  #[CLI\Command(name: 'htmx:debug', aliases: ['htmx-debug'])]
+  #[CLI\Argument(name: 'mode', description: 'Mode for enabling and disabling.')]
+  #[CLI\Usage(name: 'drush htmx:debug on', description: 'Enable debug mode on all htmx elements.')]
+  #[CLI\Usage(name: 'drush htmx:debug off', description: 'Disable debug mode on all htmx elements.')]
+  public function toggleDebug(string $mode): void {
+    /** @var \Drush\Style\DrushStyle $io */
+    $io = $this->io();
+
+    if ($mode === 'on') {
+      $this->state->set('htmx_plus.debug_enabled_all', TRUE);
+      $io->writeln('Debug enabled for all htmx elements.');
       return;
     }
-  }
 
-  /**
-   * Set or unset the hx-debug attribute on all htmx elements.
-   */
-  protected function setDebugAttribute(): void {
+    if ($mode === 'off') {
+      $this->state->set('htmx_plus.debug_enabled_all', FALSE);
+      $io->writeln('Debug disabled for all htmx elements.');
+      return;
+    }
+
+    /** @var \Psr\Log\LoggerInterface $logger */
+    $logger = $this->logger();
+    $logger->warning('Invalid mode specified. Use "on" or "off".');
   }
 
 }
--- src/EventSubscriber/HtmxDebugLibrarySubscriber.php
+++ src/EventSubscriber/HtmxDebugLibrarySubscriber.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\htmx_plus\EventSubscriber;
 
 use Drupal\Core\Render\AttachmentsInterface;
